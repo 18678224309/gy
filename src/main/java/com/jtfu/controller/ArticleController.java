8762b239b8a7e4fa53ea97b84289808cd6bd85d4
fujiantong <fujiantong@qq.com>
2019-10-23 18:08:35 +0800

    表单添加时取消 并删除其上传的图片

--- src/main/java/com/jtfu/controller/ArticleController.java
+++ src/main/java/com/jtfu/controller/ArticleController.java
@@ -18,29 +18,71 @@ public class ArticleController {
 
     @PostMapping("/uoloadImg")
     @ResponseBody
-    public R uoloadImg(@RequestPart("photos") MultipartFile photos, String url, HttpServletRequest request) throws Exception {
-        String staticPath=request.getRealPath("static");
-        SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
-        String path=staticPath+"\\image\\"+dateFormat.format(new Date());
-        File file=new File(path);
-        if(!file.exists()){
-            file.mkdir();
+    public R uoloadImg(@RequestPart("photos") MultipartFile photos,@RequestParam("url") String url, HttpServletRequest request) throws Exception {
+        File file;
+        String path="";
+        if(url.equals("first")){
+            String staticPath=request.getRealPath("static");
+            SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
+            path=staticPath+"\\image\\"+UUID.randomUUID().toString();
+            file=new File(path);
+            if(!file.exists()){
+                file.mkdir();
+            }
+            file=new File(path+"/"+ photos.getOriginalFilename());
+        }else{
+            file=new File(url+"/"+ photos.getOriginalFilename());
+            path=url;
         }
-        file=new File(path+"/"+ UUID.randomUUID().toString()+".jpg");
+
         OutputStream outputStream=new FileOutputStream(file);
         outputStream.write(photos.getBytes());
         outputStream.flush();
         outputStream.close();
+        return R.success().set("urlsPath",path);
+    }
 
-        file=new File(path);
-        List list=new ArrayList();
-        if(file.exists()&&file.isDirectory()){
-            String[] fileNames= file.list();
-            Arrays.stream(fileNames).forEach(s->{
-                list.add(s);
-            });
 
+    @PostMapping("/removeImg")
+    @ResponseBody
+    public R removeImg(@RequestParam("url")String url){
+        File file=new File(url);
+        if(file.exists()){
+            delAllFile(file);
         }
-        return R.success().set("urls",list);
+        return R.success();
+    }
+
+    /**
+     * 删除文件或文件夹
+     * @param directory
+     */
+    public static void delAllFile(File directory){
+        if (!directory.isDirectory()){
+            directory.delete();
+        } else{
+            File [] files = directory.listFiles();
+
+            // 空文件夹
+            if (files.length == 0){
+                directory.delete();
+                System.out.println("删除" + directory.getAbsolutePath());
+                return;
+            }
+
+            // 删除子文件夹和子文件
+            for (File file : files){
+                if (file.isDirectory()){
+                    delAllFile(file);
+                } else {
+                    file.delete();
+                    System.out.println("删除" + file.getAbsolutePath());
+                }
+            }
+
+            // 删除文件夹本身
+            directory.delete();
+            System.out.println("删除" + directory.getAbsolutePath());
+        }
     }
 }
