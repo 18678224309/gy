15d224c0ff47e473694e82aef0f477205c5709e2
fujiantong <fujiantong@qq.com>
2019-11-27 15:53:12 +0800

    添加代码

--- src/main/java/com/jtfu/mapper/ArticleMapper.java
+++ src/main/java/com/jtfu/mapper/ArticleMapper.java
@@ -19,8 +19,6 @@ import java.util.Map;
  */
 public interface ArticleMapper extends BaseMapper<Article> {
 
-    @Select("select a.id,title,auth,authId,money,describes,photos,createtime,status,moneynow,IFNULL(b.count,0) replyNum from article a\n" +
-            "left join (select articleid,count(1) count from message GROUP BY articleid) b on a.id=b.articleid where a.status=1 or a.status=0 order by ${order} desc limit ${begin},${end} ")
-     List<Article> getArticleListPage(@Param("begin")long begin,@Param("end")long end,@Param("order")String order);
+     List<Article> getArticleListPage(@Param("begin")long begin,@Param("end")long end,@Param("order")String order,@Param("status") String status);
 
 }
